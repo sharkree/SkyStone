9c8835a9dcba9c0f89768e052a0692d80cde81d5
Daniel Kou <daniel.j.kou@gmail.com>
2019-11-28 18:13:19 -0800

    Full Tele-op Code!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/daniel/CompleteRobotCodeDaniel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/daniel/CompleteRobotCodeDaniel.java
@@ -37,6 +37,9 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.agitari.AgitariTeamBot;
+import org.firstinspires.ftc.teamcode.agitari.AgitariTeamBot2;
+
 import static org.firstinspires.ftc.teamcode.agitari.AgitariTeamBot.ARM_POWER;
 
 
@@ -56,39 +59,15 @@ import static org.firstinspires.ftc.teamcode.agitari.AgitariTeamBot.ARM_POWER;
 @TeleOp(name="CompleteRobotCodeDaniel", group="Linear Opmode")
 //@Disabled
 public class CompleteRobotCodeDaniel extends LinearOpMode {
-
-    public double wheelFrontRightPower = 0;
-    public double wheelFrontLeftPower = 0;
-    public double wheelBackRightPower = 0;
-    public double wheelBackLeftPower = 0;
-
-    public double turbo = 0;
-
-    public DcMotor wheelFrontRight = null;
-    public DcMotor wheelFrontLeft = null;
-    public DcMotor wheelBackRight = null;
-    public DcMotor wheelBackLeft = null;
-    public Servo clutch = null;
-    public Servo grabber = null;
-    public DcMotor arm = null;
-
-
+    public AgitariTeamBot2 bot;
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
 
-    public void init(HardwareMap robot)  {
-        wheelBackLeft  = robot.get(DcMotor.class, "wheelBackLeft");
-        wheelBackRight = robot.get(DcMotor.class, "wheelBackRight");
-        wheelFrontLeft  = robot.get(DcMotor.class, "wheelFrontLeft");
-        wheelFrontRight = robot.get(DcMotor.class, "wheelFrontRight");
-    }
-
-
     @Override
-
     public void runOpMode() {
-        init(hardwareMap);
+        bot = new AgitariTeamBot2();
+        bot.init(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -97,105 +76,55 @@ public class CompleteRobotCodeDaniel extends LinearOpMode {
         runtime.reset();
 
         while (opModeIsActive()) {
+            bot.setPower(gamepad1, telemetry);
 
-            this.turbo = turbo;
-
-            double y1 = gamepad1.left_stick_x;
-            double x1 = gamepad1.right_stick_x;
-            double x2 = gamepad1.right_trigger;
-            wheelFrontRightPower = y1 + x2 - x1;
-            wheelBackRightPower = -y1 - x2 - x1;
-            wheelFrontLeftPower = y1 - x2 + x1;
-            wheelBackLeftPower = -y1 + x2 + x1;
-
-            double max = Math.max(Math.abs(wheelFrontRightPower), Math.max(Math.abs(wheelBackRightPower),
-                    Math.max(Math.abs(wheelFrontLeftPower), Math.abs(wheelBackLeftPower))));
-
-            if (max > 1.0) {
-                wheelFrontRightPower /= max;
-                wheelBackRightPower /= max;
-                wheelFrontLeftPower /= max;
-                wheelBackLeftPower /= max;
+            //Open and Close Clutch to grab foundation
+            if (gamepad1.right_bumper) {
+                bot.openClutch();
+            }
+            else if (gamepad1.left_bumper)   {
+                bot.closeClutch();
             }
 
-//            wheelFrontRightPower *= turbo;
-//            wheelBackRightPower *= turbo;
-//            wheelFrontLeftPower *= turbo;
-//            wheelBackLeftPower *= turbo;
+            //Intake wheels
+            if (gamepad1.x)  {
+                bot.startIntake();
+            }
+            else {
+                bot.stopIntake();
+            }
 
-            wheelFrontRight.setPower(wheelFrontRightPower);
-            wheelFrontLeft.setPower(wheelFrontLeftPower);
-            wheelBackRight.setPower(wheelBackRightPower);
-            wheelBackLeft.setPower(wheelBackLeftPower);
+            //lifter
+            if (gamepad2.dpad_up)  {
+                bot.liftUp();
+            }
+            else if (gamepad2.dpad_down)  {
+                bot.liftDown();
+            }
+            else  {
+                bot.stopLift();
+            }
 
-            telemetry.addData("Power wheelFrontRightPower", "Power (%.2f)", wheelFrontRightPower);
-            telemetry.addData("Power wheelFrontLeftPower", "Power (%.2f)", wheelFrontLeftPower);
-            telemetry.addData("Power wheelBackRightPower", "Power (%.2f)", wheelBackRightPower);
-            telemetry.addData("Power wheelBackLeftPower", "Power (%.2f)", wheelBackLeftPower);
+            //grabber
+            if (gamepad2.x)  {
+                bot.openGrabber();
+            }
+            else if (gamepad2.y)  {
+                bot.closeGrabber();
+            }
+
+            //The Almighty Turn Table
+            if (gamepad2.right_bumper)     {
+                bot.rotateIn();
+            }
+            else if (gamepad2.left_bumper)     {
+                bot.rotateOut();
+            }
+            //telemetry.addData("Which Bumper", "RightBumper " + gamepad1.right_bumper);
+            //telemetry.addData("Which Bumper", "LeftBumper " + gamepad1.left_bumper);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.update();
-        }
-    }
-
-    public void forward(double turbo){
-        this.turbo = turbo;
-        wheelFrontRightPower = 1 * turbo;
-        wheelFrontLeftPower = 1 * turbo;
-        wheelBackRightPower = 1 * turbo;
-        wheelBackLeftPower = 1 * turbo;
-    }
-
-    public void backwards(double turbo){
-        this.turbo = turbo;
-        wheelFrontRightPower = -1 * turbo;
-        wheelFrontLeftPower = -1 * turbo;
-        wheelBackRightPower = -1 * turbo;
-        wheelBackLeftPower = -1 * turbo;
-    }
-
-    public void strafeRight(double turbo){
-        this.turbo = turbo;
-        wheelFrontRightPower = -1 * turbo;
-        wheelFrontLeftPower = 1 * turbo;
-        wheelBackRightPower = 1 * turbo;
-        wheelBackLeftPower = -1 * turbo;
-    }
-
-    public void strafeLeft(double speed){
-        this.turbo = turbo;
-        wheelFrontRightPower = 1 * turbo;
-        wheelFrontLeftPower = -1 * turbo;
-        wheelBackRightPower = -1 * turbo;
-        wheelBackLeftPower = 1 * turbo;
-
-        // Grabber
-        if (gamepad1.x) {
-            grabber.setPosition(0);
-            telemetry.update();
-        } else if (gamepad1.y){
-            grabber.setPosition(1);
-            telemetry.update();
-        }
-
-        // Arm control
-        double armPower = 0;
-        if (gamepad1.left_stick_y != 0.0) {
-            armPower = ARM_POWER * gamepad1.left_stick_y;
-            armPower = Range.clip(armPower, -1.0, 1.0) ;
-            arm.setPower(armPower);
-            sleep(50);
-        } else if (gamepad1.a)
-            arm.setPower (0);
-
-        if (gamepad1.dpad_down)
-            clutch.setPosition(1);
-        else if (gamepad1.dpad_up)
-            clutch.setPosition(0);
-
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Arm", "Power (%.2f)", armPower);
-        telemetry.addData("Gamepad", "Stick Y (%.2f)", gamepad1.right_stick_y);
-        telemetry.update();
-
-        //sleep(1000);   // optional pause after each move(1000);
+       }
     }
 }
