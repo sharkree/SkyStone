45db68ffdd4fe65bad599645c07fd8736226a4c4
Daniel Kou <daniel.j.kou@gmail.com>
2019-12-21 11:02:19 -0800

    Full Tele-op Code!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/agitari/AgitariTeamBot2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/agitari/AgitariTeamBot2.java
@@ -81,7 +81,7 @@ public class AgitariTeamBot2
         wheelBackRight = hwMap.get(DcMotor.class, "wheel_back_right");
 
         wheelBackLeft.setDirection(DcMotor.Direction.REVERSE);
-        wheelFrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        wheelFrontLeft.setDirection(DcMotor.Direction.REVERSE);
 
         intakeLeft  = hwMap.get(DcMotor.class, "intake_left");
         intakeRight = hwMap.get(DcMotor.class, "intake_right");
